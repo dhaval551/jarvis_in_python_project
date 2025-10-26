f6b96332a40a4c5f6843e5a6f9bebdb49045a37b
dhaval551 <parvinbhaisavaliya1725@gmail.com>
2025-10-26 18:21:53 +0530

    Add files via upload

---
+++ RealTimeSearchEngine.py
@@ -0,0 +1,104 @@
+from email import message
+from googlesearch import search 
+from groq import Groq
+from json import load, dump 
+import datetime
+from dotenv import dotenv_values
+from yaml import serialize
+
+
+
+env_vars = dotenv_values(".env")
+
+Username = env_vars.get("Username")
+Assistantname = env_vars.get("Assistantname")
+GroqAPIKEY = env_vars.get("GroqAPIKEY")
+
+client = Groq(api_key=GroqAPIKEY)
+
+System = f"""Hello, I am {Username}, You are a very accurate and advanced AI chatbot named {Assistantname} which has real-time up-to-date information from the internet.
+*** Provide Answers In a Professional Way, make sure to add full stops, commas, question marks, and use proper grammar.***
+*** Just answer the question from the provided data in a professional way. ***"""
+
+try:
+     with open(r"Data\Chatlog.json","r") as f:
+        messages = load(f)
+
+except:
+      with open(r"Data\Chatlog.json","w") as f:
+       dump([],f)  
+
+def GoogleSearch(query):
+   
+        results = list(search(query,advanced=True,num_results=5))
+        Answer = f"The search results for '{query}' are:\n[start]\n"
+        for r in results:
+            Answer += f"Title: {r.title}\nDescription: {r.description}\n\n"
+        Answer += "[end]"
+        return Answer
+
+def AnswerModifier(Answer):
+    lines = Answer.split('\n')
+    non_empty_lines = [line for line in lines if line.strip()]
+    modified_answer = "\n".join(non_empty_lines)
+    return modified_answer 
+
+def Information():
+    current_date_time = datetime.datetime.now()
+    day = current_date_time.strftime("%A")
+    date = current_date_time.strftime("%d")
+    month = current_date_time.strftime("%B")
+    year = current_date_time.strftime("%Y")
+    hours = current_date_time.strftime("%H")
+    minutes = current_date_time.strftime("%M")
+    second = current_date_time.strftime("%S")
+    data = ""
+    data += f"use this real_time information if needed,\n"
+    data += f"Day: {day} \n"
+    data += f"Date: {date} \n"
+    data += f"Month: {month} \n"
+    data += f"Year: {year} \n"
+    data += f"Time {hours} ,{minutes} minutes,{second} seconds\n"
+    return data
+
+def RealtimeSearchEngine(prompt):
+    SystemChatBot = [
+        {"role": "system", "content": System},
+        {"role": "user", "content": "Hii"},
+        {"role": "assistant", "content": "Hello , how can I help you?"},
+        {"role": "system", "content": GoogleSearch(prompt)}
+    ]
+    try:
+        with open(r"Data\Chatlog.json", "r") as f:
+            messages = load(f)
+    except FileNotFoundError:
+        messages = []
+    messages.append({"role": "user", "content": f"{prompt}"})
+
+    completion = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=SystemChatBot + [{"role": "system", "content": Information()}] + messages,
+        max_tokens=2048,
+        temperature=0.7,
+        top_p=1,
+        stream=True,
+        stop=None
+    )
+
+    Answer = ""
+    for chunk in completion:
+        if chunk.choices[0].delta.content:
+            Answer += chunk.choices[0].delta.content
+
+    Answer = Answer.strip().replace("<\s>", "")
+    messages.append({"role": "assistant", "content": Answer})
+
+    with open(r"Data\Chatlog.json", "w") as f:
+        dump(messages, f, indent=4)
+
+    return AnswerModifier(Answer=Answer)
+
+if __name__ == "__main__":
+    while True:
+        prompt = input("Enter your query: ")
+        print(RealtimeSearchEngine(prompt))
