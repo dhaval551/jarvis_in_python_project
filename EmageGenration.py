599e8e152c2768380dc6b716834097176c5ac4f6
dhaval551 <parvinbhaisavaliya1725@gmail.com>
2025-10-26 18:20:58 +0530

    Add files via upload

---
+++ EmageGenration.py
@@ -0,0 +1,85 @@
+import asyncio
+from random import randint
+from PIL import Image
+import requests
+from dotenv import get_key
+import os
+from time import sleep
+
+
+def open_images(prompt):
+    folder_path = r"Data"
+    prompt = prompt.replace(" ","_")
+ 
+  
+    Files =  [f"{prompt}{i}.jpg" for i in range(1,2)]
+
+    for jpg_file in Files:
+        image_path = os.path.join(folder_path,jpg_file)
+
+        try:
+
+            img = Image.open(image_path)
+            print(f"opening images : ({image_path})")
+            img.show()
+            sleep(1)
+        except IOError:
+            print(f"unable to open{image_path}")
+
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+headers = {"Authorization": f"Bearer {get_key('.env','huggingFaceAPIkey')}"}
+
+async def query(payload):
+    response = await asyncio.to_thread(requests.post, API_URL, headers=headers, json=payload)
+    print("API status:", response.status_code)
+    if response.status_code != 200:
+        print("API error:", response.text)
+    return response.content
+
+async def generate_images(prompt: str):
+    tasks = []
+
+    for _ in range(1):
+        payload = {
+             "inputs": f"{prompt}, quality = 4K ,sharpness=max , Ultra High details ,high resolution , send = {randint(0,1000000)} ",
+         }
+        task = asyncio.create_task(query(payload))
+        tasks.append(task)
+
+    image_bytes_list = await asyncio.gather(*tasks)
+   
+    for i,image_bytes in enumerate(image_bytes_list):
+        with open(fr"Data\{prompt.replace(' ','_')}{i + 1}.jpg","wb")as f:
+            f.write(image_bytes)
+
+
+def GenerateImages(prompt: str):
+       asyncio.run(generate_images(prompt))
+       open_images(prompt)
+
+while True:
+
+     try:
+          with open(r"frontend\Files\EmageGeneration.data","r")   as f:
+              Data:str =f.read()
+          
+          Prompt, Status = Data.split(",")
+
+          if Status == "True":
+              print("generating Images....")
+              ImageStatus = GenerateImages(prompt = Prompt)
+
+              with open(r"frontend\Files\EmageGeneration.data","w") as f:
+               f.write("False,False")
+               break
+          else :      
+              sleep(1)
+
+     except  Exception as e:
+             print("Error:", e)
+         
+
+
+
+        
+
